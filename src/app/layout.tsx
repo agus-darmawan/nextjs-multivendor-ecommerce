32b812e8978ca8d4a341963cbb975ce84b19575f
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-04-13 11:39:03 +0700

    feat: add style folder and add reset.css to reset default css properties of html tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/style/globals.css";
+import "@/style/reset.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
